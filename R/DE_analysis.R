4ac0c61c5d17ddfa59f58a23a6ee9f5ec49f7688
Shreyan Gupta <61325229+Xenon8778@users.noreply.github.com>
2024-01-25 12:07:47 -0600

    Add files via upload

--- R/DE_analysis.R
+++ R/DE_analysis.R
@@ -3,8 +3,6 @@ library(Seurat)
 library(dplyr)
 library(ggplot2)
 library(readxl)
-library(ComplexHeatmap)
-library(colorRamp2)
 
 runedger = function(x,y){
   #x = Seurat Object
@@ -32,7 +30,7 @@ runedger = function(x,y){
 
 so = readRDS("output/pig_v3.rds")
 
-# DE Between Exercised and Sedentary ####
+# Between Exercised and Sedentary ####
 y='Fibroblasts'
 runedger(so,'T cells')
 runedger(so,'Beige adipocytes')
@@ -54,7 +52,7 @@ y = 'Fibroblasts'
 y = 'Erythroid cells'
 y = 'Mesenchymal cells'
 
-# Dotplots 
+
 name = paste("EdgeR/EdgeR_",y,"_new.csv",sep="")
 et = read.csv(name)
 et = et[order(et$PValue),]
@@ -75,6 +73,7 @@ DotPlot(object = soforDE_dot, features = unique(et$X),group.by = "Exercised",
   coord_flip()
 dev.off()
 
+
 y = 'Beige adipocytes'
 y = 'B cells'
 y = 'Endothelial cells'
@@ -82,7 +81,47 @@ y = 'Smooth muscle cells'
 y = 'Macrophages'
 
 
-# DE Between Occluded and Non-occluded ####
+#Marker genes
+soformarkers = subset(so,idents = c("Erythroid cells","Mesenchymal cells","Fibroblasts"), invert=TRUE)
+so.markers <- FindAllMarkers(soformarkers, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+
+marker_genes = read_xls("PigCellTypeMarkers_11232022.xls")
+labels = read_xls("PigCellTypeMarkers_11232022.xls")
+labels = labels$Annotations
+labels = labels[!is.na(labels)]
+marker_genes$Cluster = marker_genes$Cluster+1
+cell_types = c()
+for (i in marker_genes$Cluster){
+  cell_types = append(cell_types,labels[i])
+}
+marker_genes$Cluster = cell_types
+marker_genes = subset(marker_genes,marker_genes$Cluster != "Unknown")
+marker_genes = marker_genes %>%
+  group_by(Cluster) %>%
+  slice_max(n = 5, order_by = avg_log2FC)
+
+jpeg('output/Pig_Dotplot_DEGs.jpeg',width = 800, height = 1200, quality = 100,pointsize = 100)
+DotPlot(object = soformarkers, features = unique(marker_genes$M.Genesnew))+
+  ggtitle('nFeature Genes vs Cell Type')+
+  theme(axis.title = element_text(size = 25),
+        plot.title = element_text(color="red", size=30),
+        axis.text = element_text(size = 15),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        legend.text = element_text(size = 15),
+        legend.key.size = unit(1, 'cm'))+
+  coord_flip()
+dev.off()
+
+write.csv(marker_genes,"PigMarkers.csv")
+
+Idents(soformarkers)
+levels(soformarkers)
+as.character(levels(soformarkers))
+
+
+
+
+# Between Occluded and Non-occluded ####
 
 ## Exercised
 runedger = function(x,y){
@@ -132,7 +171,6 @@ y = 'Fibroblasts'
 y = 'Erythroid cells'
 y = 'Mesenchymal cells'
 
-# Dotplots 
 name = paste("EdgeR/Ex/EdgeR_",y,"_new_n-o.csv",sep="")
 et = read.csv(name)
 et = et[order(et$PValue),]
@@ -191,7 +229,6 @@ y = 'Fibroblasts'
 y = 'Erythroid cells'
 y = 'Mesenchymal cells'
 
-# Dotplots 
 name = paste("EdgeR/sed/EdgeR_",y,"_n-o.csv",sep="")
 et = read.csv(name)
 et = et[order(et$PValue),]
@@ -250,7 +287,7 @@ runedger(so,'Fibroblasts')
 runedger(so,'Erythroid cells')
 runedger(so,'Mesenchymal cells')
 
-# Dotplots 
+
 for (i in unique(so$cell_types)){
   name = paste0("EdgeR/NOcc/EdgeR_",i,"_NOcc.csv",sep="")
   et = read.csv(name)
@@ -261,7 +298,7 @@ for (i in unique(so$cell_types)){
 
   png(n1,width = 6, height = 7, res = 300, units = 'in')
   g= DotPlot(object = soforDE_dot, features = unique(et$X),group.by = "Batches",
-             scale = F, cols = c("yellow", "red"), dot.scale = 10)+
+             scale = F, cols = c("lightgrey", "#FF68A1"), dot.scale = 10)+
     ggtitle(paste(i))+
     theme(axis.title = element_text(face="bold"),
           plot.title = element_text(face="bold", hjust = 0.5))+
@@ -309,7 +346,6 @@ runedger(so,'Fibroblasts')
 runedger(so,'Erythroid cells')
 runedger(so,'Mesenchymal cells')
 
-# Dotplots 
 for (i in unique(so$cell_types)){
   if ( i != 'Mesenchymal cells'){
     print(i)
@@ -322,7 +358,7 @@ for (i in unique(so$cell_types)){
 
     png(n1,width = 6, height = 7, res = 300, units = 'in')
     g= DotPlot(object = soforDE_dot, features = unique(et$X),group.by = "Batches",
-               scale = F, cols = c("yellow", "red"), dot.scale = 10)+
+               scale = F, cols = c("lightgrey", "#FF68A1"), dot.scale = 10)+
       ggtitle(paste(i))+
       theme(axis.title = element_text(face="bold"),
             plot.title = element_text(face="bold", hjust = 0.5))+
@@ -331,8 +367,9 @@ for (i in unique(so$cell_types)){
     dev.off()
   }
 }
+# Number of DEGs plot ####
 
-# Number of DEGs Heatmap
+# Occ and Non-Occ
 y = c('Beige adipocytes','T cells','B cells','Endothelial cells',
       'Smooth muscle cells','Macrophages','Fibroblasts','Erythroid cells',
       'Mesenchymal cells')
@@ -343,22 +380,24 @@ colnames(DEG_Heat) = c('Upregulated','Downregulated','Upregulated','Downregulate
 
 for(i in 1:length(y)){
   cnt = 1
-  name = paste("EdgeR/NOcc/EdgeR_",y[i],"_NOcc.csv",sep="")
+  name = paste("EdgeR/NOcc/Revised/EdgeR_",y[i],"_NOcc_E-S.csv",sep="")
   if (file.exists(name)){
     et = read.csv(name)
-    et = et[order(et$PValue),]
-    et  = subset(et,PValue<0.01)
+    et = et %>% arrange(PValue_Adj) %>%
+      filter(PValue_Adj<0.05) %>%
+      filter(abs(logFC)>1)
     upreg = nrow(subset(et,logFC>0))
     DEG_Heat[i,1] = upreg
     downreg = nrow(subset(et,logFC<0))
     DEG_Heat[i,2] = downreg
   }
 
-  name = paste("EdgeR/Occ/EdgeR_",y[i],"_Occ.csv",sep="")
+  name = paste("EdgeR/Occ/Revised/EdgeR_",y[i],"_Occ_E-S.csv",sep="")
   if (file.exists(name)){
     et = read.csv(name)
-    et = et[order(et$PValue),]
-    et  = subset(et,PValue<0.01)
+    et = et %>% arrange(PValue_Adj) %>%
+      filter(PValue_Adj<0.05) %>%
+      filter(abs(logFC)>1)
     upreg = nrow(subset(et,logFC>0))
     DEG_Heat[i,3] = upreg
     downreg = nrow(subset(et,logFC<0))
@@ -366,13 +405,65 @@ for(i in 1:length(y)){
   }
 }
 
-# Heatmap
 DEG_Heat = na.omit(DEG_Heat)
-col_fun = colorRamp2(c(0, 5000), c("white", "#F68282"))
+
+library(ComplexHeatmap)
+library(colorRamp2)
+
+col_fun = colorRamp2(c(0, max(DEG_Heat)), c("white", "#F68282"))
+
 Heatmap(DEG_Heat, name = "DEGs",col = col_fun, cluster_columns = F, cluster_rows = F,
         cell_fun = function(j, i, x, y, width, height, fill) {
           grid.text(sprintf("%d", DEG_Heat[i, j]), x, y, gp = gpar(fontsize = 15))
         },
         border = TRUE, column_dend_reorder = FALSE,
         column_split = c("Non-Occluded","Non-Occluded","Occluded", "Occluded"), cluster_column_slices = F,
+        right_annotation = rowAnnotation(nDEGs = anno_barplot(rowSums(DEG_Heat))))
+
+# Ex and Sed
+y = c('Beige adipocytes','T cells','B cells','Endothelial cells',
+      'Smooth muscle cells','Macrophages','Fibroblasts','Erythroid cells',
+      'Mesenchymal cells')
+nrows = length(y)
+ncols = 4
+DEG_Heat = data.frame(matrix(ncol = ncols, nrow = nrows),row.names = y)
+colnames(DEG_Heat) = c('Upregulated','Downregulated','Upregulated','Downregulated')
+
+for(i in 1:length(y)){
+  cnt = 1
+  name = paste("EdgeR/Ex/Revised/EdgeR_",y[i],"_Ex_O-N.csv",sep="")
+  if (file.exists(name)){
+    et = read.csv(name)
+    et = et %>% arrange(PValue_Adj) %>%
+      filter(PValue_Adj<0.05) %>%
+      filter(abs(logFC)>1)
+    upreg = nrow(subset(et,logFC>0))
+    DEG_Heat[i,1] = upreg
+    downreg = nrow(subset(et,logFC<0))
+    DEG_Heat[i,2] = downreg
+  }
+
+  name = paste("EdgeR/Sed/Revised/EdgeR_",y[i],"_Sed_O-N.csv",sep="")
+  if (file.exists(name)){
+    et = read.csv(name)
+    et = et %>% arrange(PValue_Adj) %>%
+      filter(PValue_Adj<0.05) %>%
+      filter(abs(logFC)>1)
+    upreg = nrow(subset(et,logFC>0))
+    DEG_Heat[i,3] = upreg
+    downreg = nrow(subset(et,logFC<0))
+    DEG_Heat[i,4] = downreg
+  }
+}
+
+DEG_Heat = na.omit(DEG_Heat)
+
+col_fun = colorRamp2(c(0, max(DEG_Heat)), c("white", "#F68282"))
+
+Heatmap(DEG_Heat, name = "DEGs",col = col_fun, cluster_columns = F, cluster_rows = F,
+        cell_fun = function(j, i, x, y, width, height, fill) {
+          grid.text(sprintf("%d", DEG_Heat[i, j]), x, y, gp = gpar(fontsize = 15))
+        },
+        border = TRUE, column_dend_reorder = FALSE,
+        column_split = c("Exercised","Exercised","Sedentary", "Sedentary"), cluster_column_slices = F,
         right_annotation = rowAnnotation(nDEGs = anno_barplot(rowSums(DEG_Heat))))
